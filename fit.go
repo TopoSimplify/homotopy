485ad4c0a80cde92a2e8dac8036073d50cc81664
Titus Tienaah <dxtitus@gmail.com>
2016-08-10 11:41:27 -0300

    in dev mode

--- fit.go
+++ fit.go
@@ -6,6 +6,7 @@ import (
     . "simplex/geom"
     . "simplex/relations"
     . "simplex/struct/sset"
+    "fmt"
 )
 
 //find spatial fit
@@ -16,6 +17,7 @@ func (self *Homotopy) FindSpatialFit(i, j int) *SSet {
     vset.Add(Int(j))
 
     var pln = NewLineString(self.pln[i: j + 1])
+    fmt.Println("pln:", pln.WKT())
 
     var comparators = make([]Comparator, 0)
     for _, r := range self.relations {
@@ -23,11 +25,12 @@ func (self *Homotopy) FindSpatialFit(i, j int) *SSet {
     }
 
     var subpln = self.subgeom(vset)
+    fmt.Println("pln:\t", subpln.WKT())
+
     var isvalid = self.isvalid(subpln, comparators)
 
     for self.intpool.HasNext() && !isvalid {
         var queue = self.intpool.Next().Clone()
-
         var fixint Item
         for !isvalid && !queue.IsEmpty() {
             idx := queue.Pop().(*dp.Vertex).Index()
@@ -35,10 +38,12 @@ func (self *Homotopy) FindSpatialFit(i, j int) *SSet {
                 fixint = idx
             }
             vset.Add(idx)
+            fmt.Println("idx:\t", self.pln[int(idx)].WKT())
 
             self.DeflectionFilter(vset, idx, fixint)
 
             subpln = self.subgeom(vset)
+            fmt.Println("pln:\t", subpln.WKT())
 
             isvalid = self.isvalid(subpln, comparators)
         }
